44e6bda838dda6ff4264e7b5f0ba3d73a4a98f47
My <mdinh6@berkeley.edu>
2017-12-05 11:57:26 -0800

    fix error with income

--- FeatureSelection.R
+++ FeatureSelection.R
@@ -8,10 +8,9 @@ dir = "/Users/MyDinh/Downloads/Stat154/Stat154Project/"
 setwd(dir)
 set.seed(123)
 
-df_impute = readRDS("data/df_impute_feat.rds")
-df_impute$marital_status = as.factor(df_impute$marital_status)
-df_impute$sex = as.factor(df_impute$sex)
-
+df_impute = readRDS("data/df_remove.rds")
+df_impute = data.table(df_impute)
+saveRDS(df_impute, "data/df_remove.rds")
 
 
 n = ceiling(nrow(df_impute) * 0.8)
@@ -27,7 +26,8 @@ test_origin = df_impute[-train_idx, ]
 xtrain_origin  = train_origin[,-c("income")]
 ytrain_origin = train_origin$income
 
-
+xtest_origin  = test_origin[,-c("income")]
+ytest_origin = test_origin$income
 
 
 #######################################
@@ -40,4 +40,15 @@ rf_default  = train(x = train_origin[,-14], y = train_origin$income,
                     importance= T, ntree = 100)
 
 
-### evaluation: 
+
+
+rf_default
+pred = predict(rf_default, xtest_origin, cutoff = 0.5)
+confusionMatrix(ytest_origin, pred)
+####impute with feature: 
+#### Accuracy: train: 86 %, test = 86%, t
+### evaluation:
+# Prediction      Less.than.50k More.than.50k
+# Less.than.50k          6011           364
+# More.than.50k           792          1264
+
